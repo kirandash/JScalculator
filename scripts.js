bb7b47d97b4bcc3543e4b03aae7387f15c22d358
kirandash <thekirankumardash@gmail.com>
2017-03-12 16:47:30 +0530

    5 Tweak to check if two operators are pressed

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ var output = document.getElementById("output");
 var calcResult = '';//To store all the typed values
 var reset = false;//Boolean for reset - when = or c or 0 is pressed
 var calcSwitch = false;//Boolean for update result when calc buttons +, -, *, / are pressed
+var operators = ["+","-","*","/"];//Operators array
 
 for(var i = 0; i<btn.length; i++){
 	btn[i].addEventListener('click', function(){
@@ -14,8 +15,10 @@ for(var i = 0; i<btn.length; i++){
 		}
 
 		if(pressedVal == "="){
-			calcResult = eval(calcResult); //JS function to evaluate mathematical expression
 			reset = true;//Clear everytime = is pressed
+			if(operators.indexOf(calcResult.slice(-1)) == -1){
+				calcResult = eval(calcResult); //JS function to evaluate mathematical expression
+			}
 		}else if(pressedVal == "C"){
 			calcResult = 0;
 			reset = true;//Clear
@@ -23,12 +26,17 @@ for(var i = 0; i<btn.length; i++){
 			calcResult = calcResult +  pressedVal;
 		}
 
-		if(calcSwitch){
-			calcSwitch = false;
-			calcResult = eval(calcResult); //JS function to evaluate mathematical expression
-		}
-
 		if(pressedVal=="+"||pressedVal=="*"||pressedVal=="-"||pressedVal=="/"){
+			if(calcSwitch){
+				calcSwitch = false;
+				if(operators.indexOf(calcResult.slice(-1)) > -1){	
+					//If operator pressed twice remove the last one
+					calcResult = calcResult.substring(0,calcResult.length-1);//take a substring starting from zero to length - 1
+				}else{
+					//Check if the last character typed in not in the operators array
+					calcResult = eval(calcResult); //JS function to evaluate mathematical expression
+				}
+			}		
 			calcSwitch = true;
 		}
 		output.innerHTML = calcResult;
