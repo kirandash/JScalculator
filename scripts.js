12c8e9bccafa1b1568d0851f6d0846b8518b60c8
kirandash <thekirankumardash@gmail.com>
2017-03-12 14:15:39 +0530

    3 Reset for =, c and 0 before numbers

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
 //Add eventlistener for all 16 buttons
-var myBtn = document.getElementsByClassName("cbutton");
+var btn = document.getElementsByClassName("cbutton");
 var output = document.getElementById("output");
-var myCal = '';//To store all the typed values
+var calcResult = '';//To store all the typed values
+var reset = false;//Boolean for reset
 
-for(var i = 0; i<myBtn.length; i++){
-	myBtn[i].addEventListener('click', function(){
+for(var i = 0; i<btn.length; i++){
+	btn[i].addEventListener('click', function(){
 		var myValue = this.innerHTML;
+		if(reset || calcResult == "0"){
+			reset = false;
+			calcResult = '';
+		}
 
-		myCal = myCal +  myValue;
-		output.innerHTML = myCal;
+		if(myValue == "="){
+			calcResult = eval(calcResult); //JS function to evaluate mathematical expression
+			reset = true;//Clear everytime = is pressed
+		}else if(myValue == "C"){
+			calcResult = 0;
+			reset = true;//Clear
+		}else{
+			calcResult = calcResult +  myValue;
+		}
+		output.innerHTML = calcResult;
 	});
 }
