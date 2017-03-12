106531984913531a418d6124886a9e624316381a
kirandash <thekirankumardash@gmail.com>
2017-03-12 15:32:13 +0530

    4 onclick calculations

--- scripts.js
+++ scripts.js
@@ -2,24 +2,34 @@
 var btn = document.getElementsByClassName("cbutton");
 var output = document.getElementById("output");
 var calcResult = '';//To store all the typed values
-var reset = false;//Boolean for reset
+var reset = false;//Boolean for reset - when = or c or 0 is pressed
+var calcSwitch = false;//Boolean for update result when calc buttons +, -, *, / are pressed
 
 for(var i = 0; i<btn.length; i++){
 	btn[i].addEventListener('click', function(){
-		var myValue = this.innerHTML;
+		var pressedVal = this.innerHTML;
 		if(reset || calcResult == "0"){
 			reset = false;
 			calcResult = '';
 		}
 
-		if(myValue == "="){
+		if(pressedVal == "="){
 			calcResult = eval(calcResult); //JS function to evaluate mathematical expression
 			reset = true;//Clear everytime = is pressed
-		}else if(myValue == "C"){
+		}else if(pressedVal == "C"){
 			calcResult = 0;
 			reset = true;//Clear
 		}else{
-			calcResult = calcResult +  myValue;
+			calcResult = calcResult +  pressedVal;
+		}
+
+		if(calcSwitch){
+			calcSwitch = false;
+			calcResult = eval(calcResult); //JS function to evaluate mathematical expression
+		}
+
+		if(pressedVal=="+"||pressedVal=="*"||pressedVal=="-"||pressedVal=="/"){
+			calcSwitch = true;
 		}
 		output.innerHTML = calcResult;
 	});
