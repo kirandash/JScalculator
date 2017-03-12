d7da1743f40ff86048cd823d2ead193b49cb4b82
kirandash <thekirankumardash@gmail.com>
2017-03-12 13:27:32 +0530

    2 Add input to screen

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 //Add eventlistener for all 16 buttons
 var myBtn = document.getElementsByClassName("cbutton");
-console.log(myBtn);
+var output = document.getElementById("output");
+var myCal = '';//To store all the typed values
+
 for(var i = 0; i<myBtn.length; i++){
 	myBtn[i].addEventListener('click', function(){
-		console.log('clicked');
+		var myValue = this.innerHTML;
+
+		myCal = myCal +  myValue;
+		output.innerHTML = myCal;
 	});
 }
